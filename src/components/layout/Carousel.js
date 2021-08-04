3e3b658381122269d30f202b5345b7f496acc0a6
Richa Pandey <81300346+RichaPandey02@users.noreply.github.com>
2021-08-04 12:41:43 +0530

    slidebar added

--- src/components/layout/Carousel.js
+++ src/components/layout/Carousel.js
@@ -1,40 +1,142 @@
 import React from "react";
 import Carousel from "react-grid-carousel";
  import carousl from "../styles/carousl.css";
+ import $ from 'jquery';
+
+
+ 
 
 const Carousel1 = () => {
+//   var slides = document.querySelectorAll(".slide");
+// var dots = document.querySelectorAll(".dot");
+// var index = 0;
+
+
+// function prevSlide(n){
+//   index+=n;
+//   console.log("prevSlide is called");
+//   changeSlide();
+// }
+
+// function nextSlide(n){
+//   index+=n;
+//   changeSlide();
+// }
+
+// changeSlide();
+
+// function changeSlide(){
+    
+//   if(index>slides.length-1)
+//     index=0;
+  
+//   if(index<0)
+//     index=slides.length-1;
+  
+  
+  
+//     for(let i=0;i<slides.length;i++){
+//       slides[i].style.display = "none";
+      
+//       dots[i].classList.remove("active");
+      
+      
+//     }
+    
+//     slides[index].style.display = "block";
+//     dots[index].classList.add("active");
+
+  
+
+// }
+
+
+
+  
+
   return (
-      <Carousel cols={3} rows={1} gap={10} loop>
-        <Carousel.Item>
-          <img width="100%" src="https://wallpaperaccess.com/full/1154374.jpg" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width="100%" src="https://wallpapercave.com/wp/wp4516338.jpg" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            width="100%"
-            height="100%"
-            src="https://media.istockphoto.com/photos/rural-landscape-sunset-panorama-picture-id487300412?k=6&m=487300412&s=170667a&w=0&h=V2OFI_UQyTNBC1VUwClkSpe1uQLs_ch_mFTOBnAzEgE="
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width="100%" src="https://wallpaperaccess.com/full/1154374.jpg" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width="100%" src="https://wallpapercave.com/wp/wp4516338.jpg" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width="100%" src="https://wallpaperaccess.com/full/1154374.jpg" />
-        </Carousel.Item>
-      </Carousel>
+      // <Carousel cols={3} rows={1} gap={10} loop>
+      //   <Carousel.Item>
+      //     <img width="100%" src="https://wallpaperaccess.com/full/1154374.jpg" />
+      //   </Carousel.Item>
+      //   <Carousel.Item>
+      //     <img width="100%" src="https://wallpapercave.com/wp/wp4516338.jpg" />
+      //   </Carousel.Item>
+      //   <Carousel.Item>
+      //     <img
+      //       width="100%"
+      //       height="100%"
+      //       src="https://media.istockphoto.com/photos/rural-landscape-sunset-panorama-picture-id487300412?k=6&m=487300412&s=170667a&w=0&h=V2OFI_UQyTNBC1VUwClkSpe1uQLs_ch_mFTOBnAzEgE="
+      //     />
+      //   </Carousel.Item>
+      //   <Carousel.Item>
+      //     <img width="100%" src="https://wallpaperaccess.com/full/1154374.jpg" />
+      //   </Carousel.Item>
+      //   <Carousel.Item>
+      //     <img width="100%" src="https://wallpapercave.com/wp/wp4516338.jpg" />
+      //   </Carousel.Item>
+      //   <Carousel.Item>
+      //     <img width="100%" src="https://wallpaperaccess.com/full/1154374.jpg" />
+      //   </Carousel.Item>
+      // </Carousel>
+
+      
+     
+      
 
 
 
 
 
 
+      // <div>
+      // <div id="slider">  
+      //   <div class="slide" style={{background:'red'}}>
+      //     <img src="https://img.fotocommunity.com/peacefull-95e65145-d050-4957-9ddc-85dd60689d45.jpg?height=500" >
+      //       </img>
+      //   </div>
+        
+      //   <div class="slide" style={{background:'coral'}}>
+      //      <img src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/2/sunset-storm-haystack-rock-canon-beach-oregon-coast-tl-mair.jpg" >
+      //        </img>
+      //   </div>
+       
+      //   <div class="slide" style={{background:'crimson'}}>
+      //     <img src=" https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/1-tourists-at-eiffel-tower-paris-panoramic-images.jpg" >
+      //       </img>
+      //   </div>
+        
+      //   <div class="slide" style={{background:'#6edf10'}}>
+      //     <img src="https://dmasia.travel/wp-content/uploads/2019/09/vit3-1000x350.jpg" >
+      //       </img>
+      //   </div>
+        
+      
+      //   <span class="controls" onclick="prevSlide(-1)" id="left-arrow"><i class="fa fa-arrow-left" aria-hidden="true"></i>
+      // </span>
+      //   <span class="controls" id="right-arrow" onclick="nextSlide(1)"><i class="fa fa-arrow-right" aria-hidden="true"></i>
+      // </span>
+      // </div>
+      //   <div id="dots-con">
+      //  <span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>
+      //  </div>
+      //  </div>
+      
+       
+
+
+
+      <div id="slider">
+					  <figure>
+						    <img src="https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701663711.jpg"></img>
+						    <img src="https://lh3.googleusercontent.com/proxy/MHqFKXRFs_tO8bvgaEs8cQbQfz25SwgjFuHS5Zg2pLdL4bsecqCijocQkR8IaLpNN6bHg34z718HVTM6Vx3Ynsg7NCfRTbMQvk0mhYNsqxUV-Dtq6PjnKZerdSUfzub6kSyU"></img>
+						    <img src="https://wallpaperaccess.com/full/1541457.jpg"></img>
+					  </figure>
+				</div>
+      
    
+
+ 
   );
 };
 export default Carousel1;
