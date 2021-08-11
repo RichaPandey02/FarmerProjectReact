aaea27872ab5a6a94e2bab2d721132618808612d
Richa Pandey <81300346+RichaPandey02@users.noreply.github.com>
2021-08-11 13:02:48 +0530

    test

--- src/testing/Navbar.test.js
+++ src/testing/Navbar.test.js
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 import Navbar from "../components/layout/Navbar";
 import { BrowserRouter } from "react-router-dom";
 
-test("to check whether AllMovies component rendered", () => {
+test("to check whether Navbar component rendered", () => {
   render(
     <BrowserRouter>
       <Navbar />
